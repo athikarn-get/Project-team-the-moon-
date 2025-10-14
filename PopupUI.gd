c1e7ae3d3c442d729f8d7ae2c56147070cff837a
Mean442 <phichaiyutkhawboon@gmail.com>
2025-10-14 23:49:47 +0700

    new_update_14_10_2006

---
+++ PopupUI.gd
@@ -0,0 +1,25 @@
+extends PopupPanel
+
+@onready var label = $Label
+@onready var close_button = $Button
+@onready var color_rect = get_node_or_null("../ColorRect")  # now linked to ColorRect
+
+func _ready():
+	add_to_group("ui")
+	close_button.pressed.connect(_on_close_pressed)
+	hide()
+	if color_rect:
+		color_rect.visible = false
+
+func show_popup(title: String, desc: String):
+	label.bbcode_enabled = true
+	label.text = "[b]" + title + "[/b]\n" + desc
+	popup_centered()
+	show()
+	if color_rect:
+		color_rect.visible = true
+
+func _on_close_pressed():
+	hide()
+	if color_rect:
+		color_rect.visible = false
